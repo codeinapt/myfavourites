1bf0851da675aade2e22aee9ed6ab0f88948f169
codeinapt <jlgr1994@gmail.com>
2021-08-22 09:35:46 -0500

    project and database creation

---
+++ resources/views/project/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.plantilla')
+
+@section('content')
+    <h1>Bienvenido a tu pagina de favoritos!</h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyFavourites.</title>
+    <!-- favicon -->
+    <!-- estilos -->
+</head>
+<body>
+    <!-- header -->
+    <!-- nav -->
+    
+    <!-- footer -->
+
+    <!--script -->
+</body>
+</html>
